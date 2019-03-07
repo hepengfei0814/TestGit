04a31fb28f68565d0c9378418e9f2b06f3c52e97
hepengfei <252281711@qq.com>
2019-03-07 22:10:23 +0800

    second commit

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,6 +1,12 @@
 public class HelloWord {
 
     public static void main(String[] args) {
+        add();
         System.out.println("Hello World!");
+    }
+
+    public static void add(){
+        int a=1,b=2;
+        System.out.println(a+b);
     }
 }
