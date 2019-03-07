2cae435945c4caa68c1394e4615c22f3879e1d54
hepengfei <252281711@qq.com>
2019-03-08 01:20:55 +0800

    third commit

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,11 @@
 public class Test {
     public static void main(String[] args) {
+        add();
         System.out.println("Test Git!");
+    }
+
+    public static void add(){
+        int a=5,b=10;
+        System.out.println(a+b);
     }
 }
